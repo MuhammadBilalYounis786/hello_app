d6605fdb3ae0a73858f4fe703bc3883f0c6d2a43
MuhammadBilal <muhammad.bilal@devsinc.com>
2020-07-22 13:13:34 +0500

    hola mundo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #root 'controller_name#action_name', this will come in place of rails home page
-  #root 'application#hello'
-  root 'application#goodbye'
+  root 'application#hello'
+  #root 'application#goodbye'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
